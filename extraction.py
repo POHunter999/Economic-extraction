46225bfe1c5e1b4bda77fef579232ca5c854f7b4
xyzhang16 <404338476@qq.com>
2017-02-28 16:18:36 +0800

    .

---
+++ extraction.py
@@ -0,0 +1,28 @@
+__author__ = 'Administrator'
+
+
+
+
+def get_data_html( html ):
+    '''
+    dasdas
+    :param html: file_name
+    :return: text_data
+    '''
+
+    pass
+
+def get_data_category(data):
+    # data: text_data
+    # return :
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
