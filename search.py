2d905969a4f0cfcc6a3333ef95c82cb394cbb493
POHunter999 <wyhlovenudt@163.com>
2017-03-02 10:58:59 +0800

    生成一个稀疏表

--- search.py
+++ search.py
@@ -4,10 +4,12 @@ Created on Tue Feb 28 10:31:55 2017
 
 @author: POHunter
 """
-sentences=" 2015年，全省加快转变农业发展方式，全力打造高原特色现代农业，全年全省农林牧渔业总产值保持了稳定增长，粮食产量再创新高。2015年全省农林牧渔业总产值完成3383.09亿元，同比增长6.0%；完成增加值2098.19亿元，增加值总量在全国排第14位，同比增长6.0%，其中：农业增加值1230.75亿元，增长5.7%；林业增加值216.11亿元，增长10.3%；牧业增加值559.98亿元，增长4.3%；渔业增加值48.88亿元，增长8.8%。农林牧渔服务业增加值42.48亿元，增长10.2%。2015年，全省粮食生产实现了总量增及单产同步增长态势，粮食产量为1876.4万吨，比2014年增产15.7万吨，增长0.84%，实现连续十三年增长粮食综合平均单产278.8公斤，比2014年增加3.6公斤，同比增长1.3%。2015年，全省高原特色农业形势良好，特色经济作物量效齐升，茶叶、水果、花卉等价格上涨明显，橡胶、咖啡及核桃种植面积和产量继续保持全国第一，粮油、肉类、蔬菜、水产品等都获得较好收成。"
+#sentences=" 2015年，全省加快转变农业发展方式，全力打造高原特色现代农业，全年全省农林牧渔业总产值保持了稳定增长，粮食产量再创新高。2015年全省农林牧渔业总产值完成3383.09亿元，同比增长6.0%；完成增加值2098.19亿元，增加值总量在全国排第14位，同比增长6.0%，其中：农业增加值1230.75亿元，增长5.7%；林业增加值216.11亿元，增长10.3%；牧业增加值559.98亿元，增长4.3%；渔业增加值48.88亿元，增长8.8%。农林牧渔服务业增加值42.48亿元，增长10.2%。2015年，全省粮食生产实现了总量增及单产同步增长态势，粮食产量为1876.4万吨，比2014年增产15.7万吨，增长0.84%，实现连续十三年增长粮食综合平均单产278.8公斤，比2014年增加3.6公斤，同比增长1.3%。2015年，全省高原特色农业形势良好，特色经济作物量效齐升，茶叶、水果、花卉等价格上涨明显，橡胶、咖啡及核桃种植面积和产量继续保持全国第一，粮油、肉类、蔬菜、水产品等都获得较好收成。"
 import pandas as pd
 import jieba
 import jieba.posseg as pseg
+import jieba.analyse
+jieba.initialize()
 jieba.load_userdict("cidian.txt") # file_name为自定义词典的路径
 
 def cut_sentence(words):
@@ -55,18 +57,18 @@ def findData(fenci,flag):
     the="".join(flag)
     
     nmm=findStrPosition(the,'nmm')
-    nmmDF=pd.DataFrame(nmm)
+    nmmDF=pd.DataFrame(nmm,columns=["location"])
     nmmDF['type']='nmm'
         
-    vmx=findStrPosition(the,'vmx')
-    vmxDF=pd.DataFrame(vmx)
-    vmxDF['type']='vmx'
+    #vmx=findStrPosition(the,'vmx')
+    #vmxDF=pd.DataFrame(vmx,columns=["location"])
+    #vmxDF['type']='vmx'
            
     nvmm=findStrPosition(the,'vmm')
-    nvmmDF=pd.DataFrame(nvmm)
+    nvmmDF=pd.DataFrame(nvmm,columns=["location"])
     nvmmDF['type']='nvmm'
     
-    final=(nmmDF.append(vmxDF)).append(nvmmDF)
+    final=(nmmDF.append(nvmmDF))
     '''
     for i in nmm:
         print("".join(fenci[i-2:i+3]))
@@ -82,32 +84,62 @@ def main(sentences):
     a=pseg.cut(sentences)
     fenci=[]
     flag=[]
+    df=pd.DataFrame()
     for w in a:
         fenci.append(w.word)
-        flag.append(w.flag[0])
-
-    final=findData(fenci,flag)
-    final=final.sort_values([0])
-    final.columns=["location","type"]
-
+        flag.append(w.flag[0])   
+    final=findData(fenci,flag)    
     for line in final.itertuples():
-        if line[2]=='nmm':
-            print("".join(fenci[line[1]-2:line[1]+3]))
-        elif line[2]=='vmx':
-            print("".join(fenci[line[1]:line[1]+3]))
-        else:
-            print("".join(fenci[line[1]-2:line[1]+4]))
-           
-
-file = open('input.txt','r')
-words = file.readlines()
-aaa = cut_sentence(words)
-for i in aaa:
-    main(str(i))
-
         
-            
-    
-    
-    
+        zhuti1="".join(fenci[int(line[1])-2:int(line[1])])
+        zhuti2="".join(fenci[int(line[1]):int(line[1]+1)])
+        num="".join(fenci[int(line[1])+1:int(line[1])+3])
+        tag1=jieba.analyse.extract_tags(zhuti1,1)
+        tag2=jieba.analyse.extract_tags(zhuti2,1)
+        
+        if len(tag1)==1 and len(tag2)==1:            
+            temp=pd.DataFrame([[ tag1[0],num]],columns=["项目", tag2[0] ])
+            df=df.append(temp)
 
+        '''
+        if line[2]=='nmm':
+            zhuti="".join(fenci[int(line[1])-3:int(line[1])+1])
+            num="".join(fenci[int(line[1])+1:int(line[1])+3])
+            tags=jieba.analyse.extract_tags(zhuti,2)                                
+            #print("".join(fenci[int(line[1])-3:int(line[1])+3]) + 'nmm' )
+            temp=pd.DataFrame([[ str(tags[1]),num]],columns=["项目", tags[0] ])
+            df=df.append(temp)
+        elif line[2]=='nvmm':                
+            zhuti="".join(fenci[int(line[1])-3:int(line[1])+1])
+            num="".join(fenci[int(line[1])+1:int(line[1])+3])
+            tags=jieba.analyse.extract_tags(zhuti,2)                
+            #print("".join(fenci[int(line[1])-2:int(line[1])+3])+'nvmm')
+            temp=pd.DataFrame([[ str(tags[1]),num]],columns=["项目", tags[0]  ])  
+            df=df.append(temp)
+        elif line[2]=='vmx':
+            print("".join(fenci[int(line[1]):int(line[1])+3])+'vmx')
+        '''
+    return  df
+           
+def not_empty(s): return s and s.strip()
+
+diction=['增加值', '增长', '支出', '增产','']
+file = open('input.txt','r')
+text = file.readlines()
+text=list(filter(not_empty, text))
+sent=[]
+'''
+for words in text:
+    aaa=cut_sentence(words)
+    sent = sent+aaa
+'''
+dff=pd.DataFrame()
+for sentences in text:    
+    dff=dff.append(main(str(sentences)))
+    
+first = dff['项目']
+dff.drop(labels=['项目'], axis=1,inplace = True)
+dff.insert(0, '项目', first)
+dff.to_csv("dff.csv")
+        
+    
